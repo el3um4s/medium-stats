428a79fbb96cae30ffa4829d681f820945bc6ad7
Samuele de Tomasi <el3um4s@gmail.com>
2022-01-06 15:45:43 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import "./css/tailwind.pcss";
+
+  export let name: string;
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <p>Version: 0.0.3</p>
+</main>
+
+<style lang="postcss">
+  h1 {
+    @apply border-b-4;
+  }
+
+  h1:hover {
+    border-color: rgb(255, 136, 0);
+  }
+
+  a {
+    @apply p-1 rounded-lg;
+  }
+
+  a:hover {
+    border-bottom: 2px;
+    background-color: theme("colors.red.600");
+    color: theme("colors.gray.300");
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+/// <reference path="../node_modules/svelte/types/runtime/index.d.ts" />
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
+});
+
+export default app;
+
+
+
+// see this: https://github.com/sveltejs/template/issues/270#issuecomment-973031514
+// problem with typescript 4.5
